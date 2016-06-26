d2f4f3e975befd36c3df19c6d311088c6506ffc5
morow93 <derchepur@gmail.com>
2016-06-27 00:52:05 +0300

    fix

--- www/js/pages/details/details.controller.js
+++ www/js/pages/details/details.controller.js
@@ -4,28 +4,29 @@
 
     angular.module("pages.details").controller("DetailsController", detailsController);
 
-    detailsController.$inject = ["$q", "$scope", "$ionicPopup", "FriendService", "AuthService", "ToastService", "PlaylistService", "$state", "$timeout", "$stateParams", "AudioService", "BaseHttpService"];
+    detailsController.$inject = ["$q", "$scope", "$ionicPopup", "FriendService", "AuthService", "ToastService",
+    "PlaylistService", "$state", "$timeout", "$stateParams", "AudioService", "BaseHttpService"];
 
-    function detailsController($q, $scope, $ionicPopup, friendService, authService, toastService, playlistService, $state, $timeout, $stateParams, audioService, baseHttpService) {
+    function detailsController($q, $scope, $ionicPopup, friendService, authService, toastService,
+      playlistService, $state, $timeout, $stateParams, audioService, baseHttpService) {
 
       $scope.activate = activate;
 
-      $scope.$on("$ionicView.enter", function(e) {
-        activate();
-      });
-
       function activate() {
 
+        $scope.loaded = false;
+
         if (!angular.isDefined($stateParams.playlistId)) {
+          $scope.loaded = true;
           toastService.show("Can not load playlist");
           // Stop the ion-refresher from spinning
           $scope.$broadcast('scroll.refreshComplete');
           return;
         };
 
-        $scope.loaded = false;
         $scope.playlist = playlistService.get($stateParams.playlistId);
         if (!$scope.playlist || !$scope.playlist.friends) {
+          $scope.loaded = true;
           toastService.show("Can not load playlist");
           // Stop the ion-refresher from spinning
           $scope.$broadcast('scroll.refreshComplete');
@@ -39,7 +40,7 @@
 
         baseHttpService.run(function(){
           return functions;
-        }).then(function(res) {
+        }).then(function(res) {     
           $scope.audios = res;
         }).catch(function(err) {
           toastService.show("Can not load playlist");
